490768c4f396dcaab6677fddf522cc6a6927f73f
Gustavo Dias <gustavogdcosta@hotmail.com>
2024-06-06 10:16:36 -0300

    Primeiro Commit

---
+++ src/br/com/gustavo/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.gustavo;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		
+	}
+
+}
